30664cc4b84e1debf7ce8f9ea4e4237e269405fc
Ondřej Kostík <ondrakostik@gmail.com>
2022-03-17 17:28:30 +0100

    gui files introduced

---
+++ gui.c
@@ -0,0 +1,9 @@
+/*
+ * gui.c
+ *
+ *  Created on: 17 Mar 2022
+ *      Author: ondra
+ */
+
+
+#include "gui.h"
---
+++ gui.h
@@ -0,0 +1,15 @@
+/*
+ * gui.h
+ *
+ *  Created on: 17 Mar 2022
+ *      Author: ondra
+ */
+
+#ifndef GUI_H_
+#define GUI_H_
+
+
+
+
+
+#endif /* GUI_H_ */
