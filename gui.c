c2bee37f2c47cd3141955c06fb0125aff94003e2
Ondřej Kostík <xkostik@fi.muni.cz>
2022-03-23 22:11:02 +0100

    Button struct implemented; most of FIND GUI implemented

--- gui.c
+++ gui.c
@@ -7,25 +7,66 @@
 #include "gui.h"
 #include <stdbool.h>
 #include <stdio.h>
+#include "communication.h"
+
+button to_menu_button = {
+    .coords = {.xMin = 248, .yMin = 2, .xMax = 318, .yMax = 37},
+    .button_color = GRAPHICS_COLOR_LIGHT_GRAY,
+    .text_color = GRAPHICS_COLOR_BLACK,
+    .font = &g_sFontCm22b,
+    .text = "Menu",
+    .active = false
+};
+
+button find_accept_button = {
+    .coords = {.xMin = 50, .yMin = 180, .xMax = 150, .yMax = 220},
+    .button_color = GRAPHICS_COLOR_LIGHT_GRAY,
+    .text_color = GRAPHICS_COLOR_BLACK,
+    .font = &g_sFontCm24b,
+    .text = "OK",
+    .active = false
+};
+
+button find_reject_button = {
+    .coords = {.xMin = 170, .yMin = 180, .xMax = 270, .yMax = 220},
+    .button_color = GRAPHICS_COLOR_LIGHT_GRAY,
+    .text_color = GRAPHICS_COLOR_BLACK,
+    .font = &g_sFontCm24b,
+    .text = "Next",
+    .active = false
+};
 
 
 void update_display(context_state context) {
+    Graphics_setBackgroundColor(&g_context, GRAPHICS_COLOR_BLACK);
+    Graphics_setForegroundColor(&g_context, GRAPHICS_COLOR_WHITE);
     switch (context) {
     case FIND:
         Graphics_clearDisplay(&g_context);
-        _update_display_find_lookup();
-        Graphics_flushBuffer(&g_context);
+        _update_display_find_lookup(comm_state == MESSAGE_RECEIVED);
         break;
     }
 }
 
-void _update_display_find_lookup() {
+void _update_display_find_lookup(bool found) {
     int8_t string_buffer[30];
-    Graphics_setBackgroundColor(&g_context, GRAPHICS_COLOR_BLACK);
-    Graphics_setForegroundColor(&g_context, GRAPHICS_COLOR_WHITE);
-    Graphics_setFont(&g_context, &g_sFontCm22);
-    Graphics_drawStringCentered(&g_context, "Device lookup", -1, 160, 15, false);
-    Graphics_drawLineH(&g_context, 1, 320, 30);
+    Graphics_setFont(&g_context, &g_sFontCm24);
+    Graphics_drawString(&g_context, "Device lookup", -1, 4, 9, false);
+    Graphics_drawLineH(&g_context, 1, 320, 40);
     sprintf((char *) &string_buffer,  "Scanning address: 0x%02x", device_id);
-    Graphics_drawStringCentered(&g_context, string_buffer, -1, 160, 100, false);
+    Graphics_drawStringCentered(&g_context, string_buffer, -1, 160, 90, false);
+    draw_button(&g_context, &to_menu_button);
+    if (found) {
+        draw_button(&g_context, &find_accept_button);
+        draw_button(&g_context, &find_reject_button);
+    }
+}
+
+void draw_button(Graphics_Context *g_context, button *b) {
+    b->active = true;
+    Graphics_setForegroundColor(g_context, b->button_color);
+    Graphics_fillRectangle(g_context, &(b->coords));
+    Graphics_setForegroundColor(g_context, b->text_color);
+    Graphics_setFont(g_context, b->font);
+    Graphics_drawStringCentered(g_context, b->text, -1, (b->coords.xMax + b->coords.xMin) / 2, (b->coords.yMax + b->coords.yMin) / 2, false);
 }
--- gui.h
+++ gui.h
@@ -18,11 +18,31 @@ extern const Graphics_Display_Functions Kentec_fxns;
 extern Graphics_Display Kentec_GD;
 Graphics_Context g_context;
 
+
+/* This struct is used to represent buttons */
+typedef struct {
+    Graphics_Rectangle coords;
+    int32_t button_color;
+    int32_t text_color;
+    const Graphics_Font *font;
+    int8_t *text;
+    bool active; // currently displayed -> should accept clicks
+} button;
+
+// Always present "Menu" button
+extern button to_menu_button;
+
+// FIND context optional buttons
+extern button find_accept_button;
+extern button find_reject_button;
+
 // Updates the display content based on the given context
 void update_display(context_state context);
 
 /* Update/redraw display with specific screen. Used internally by update_display() */
-void _update_display_find_lookup();
+void _update_display_find_lookup(bool found);
 // TODO: Add other contexts
+
+void draw_button(Graphics_Context *g_context, button *b);
 
 #endif /* GUI_H_ */
