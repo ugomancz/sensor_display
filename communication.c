0e086bec20944e49940e8cd36502119ffa0e2333
Ondřej Kostík <xkostik@fi.muni.cz>
2022-04-04 21:25:26 +0200

    frame code removed

--- communication.c
+++ communication.c
@@ -9,39 +9,10 @@
 #include "crc.h"
 #include "application.h"
 
-frame create_frame(uint8_t slave_address, uint8_t function_code, uint8_t *data) {
-    frame f;
-    f.slave_address = slave_address;
-    f.function_code = function_code;
-    f.data_length = (uint16_t) (sizeof(data) / sizeof(uint8_t));
-    f.crc = to_modbus_compatible(get_frame_crc(f.slave_address, f.function_code, data, f.data_length));
-    f.data = calloc(f.data_length, sizeof(uint8_t));
-    for (int i = 0; i < f.data_length; i++) {
-        f.data[i] = data[i];
-    }
-    return f;
-}
-
-int uart_send_frame(uint32_t uart_base, frame message) {
-    UARTCharPut(uart_base, message.slave_address);
-    UARTCharPut(uart_base, message.function_code);
-    for (int i = 0; i < message.data_length; i++) {
-        UARTCharPut(uart_base, message.data[i]);
-    }
-    UARTCharPut(uart_base, (uint8_t) (message.crc >> 8));
-    UARTCharPut(uart_base, (uint8_t) message.crc);
-    return 0;
-}
 
 void set_direction(direction dir) {
     GPIOPinWrite(GPIO_PORTH_BASE, GPIO_PIN_4, (dir == TRANSMIT) ? 0xFF : 0x00);
 }
-
-bool parse_incoming_message(frame *f) {
-    uint16_t crc = get_crc(buffer, buffer_position - 2);
-    return true;
-}
-
 
 void send_message() {
     set_direction(TRANSMIT);
--- communication.h
+++ communication.h
@@ -22,20 +22,9 @@ typedef enum {
     MESSAGE_RECEIVED
 } comm_states;
 
-typedef struct {
-    uint8_t slave_address;
-    uint8_t function_code;
-    uint8_t *data;
-    uint16_t data_length;
-    uint16_t crc;
-} frame;
-
 extern volatile comm_states comm_state;
 
-frame create_frame(uint8_t slave_address, uint8_t function_code, uint8_t *data);
 void set_direction(direction dir);
-int uart_send_frame(uint32_t uart_base, frame message);
-bool parse_incoming_message(frame *f);
 
 /* Top level function to send message to the sensor based on the current context */
 void send_message();
