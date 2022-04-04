27aed79a30e9a952ed8a83bf8532407389278e6a
Ondřej Kostík <xkostik@fi.muni.cz>
2022-04-05 01:37:39 +0200

    MENU GUI implemented (no touch)

--- gui.c
+++ gui.c
@@ -7,6 +7,7 @@
 #include "gui.h"
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include "communication.h"
 #include "ti/devices/msp432e4/driverlib/driverlib.h"
 #include "application.h"
@@ -38,6 +39,33 @@ button find_reject_button = {
     .text_color = GRAPHICS_COLOR_BLACK,
     .font = &g_sFontCm24b,
     .text = "No",
+    .active = false
+};
+
+button menu_dose_button = {
+    .coords = {.xMin = 4, .yMin = 180, .xMax = 102, .yMax = 220},
+    .button_color = GRAPHICS_COLOR_LIGHT_GRAY,
+    .text_color = GRAPHICS_COLOR_BLACK,
+    .font = &g_sFontCm20b,
+    .text = "Dose",
+    .active = false
+};
+
+button menu_dose_rate_button = {
+    .coords = {.xMin = 110, .yMin = 180, .xMax = 208, .yMax = 220},
+    .button_color = GRAPHICS_COLOR_LIGHT_GRAY,
+    .text_color = GRAPHICS_COLOR_BLACK,
+    .font = &g_sFontCm20b,
+    .text = "Dose Rate",
+    .active = false
+};
+
+button menu_find_button = {
+    .coords = {.xMin = 216, .yMin = 180, .xMax = 316, .yMax = 220},
+    .button_color = GRAPHICS_COLOR_LIGHT_GRAY,
+    .text_color = GRAPHICS_COLOR_BLACK,
+    .font = &g_sFontCm20b,
+    .text = "Find Dev.",
     .active = false
 };
 
@@ -95,6 +123,7 @@ void _init_display_find() {
     Graphics_setForegroundColor(&g_context, GRAPHICS_COLOR_WHITE);
     Graphics_drawString(&g_context, "Device lookup", -1, 4, 9, false);
     Graphics_drawLineH(&g_context, 1, 320, 40);
+    Graphics_setFont(&g_context, &g_sFontCm22b);
     Graphics_drawString(&g_context, "Scanning address:", -1, 4, 60, false);
     sprintf((char *) &string_buffer, "0x%02x", device_address);
     Graphics_drawString(&g_context, string_buffer, -1, 220, 60, false);
@@ -118,10 +147,11 @@ void _update_display_find(bool found) {
 
         Graphics_setFont(&g_context, &g_sFontCm20);
 
-        sprintf((char *) &string_buffer,  "Name: %s", switch_endianity(device_id.pr_name));
+        sprintf((char *) &string_buffer,  "Name: %s", device_id.pr_name);
         Graphics_drawString(&g_context, string_buffer, -1, 4, 120, false);
 
-        sprintf((char *) &string_buffer,  "HW ID: %d", device_id.ser_no);
+        sprintf((char *) string_buffer, "HW ID: ");
+        format_hw_id(string_buffer, device_id.hw_id);
         Graphics_drawString(&g_context, string_buffer, -1, 4, 140, false);
 
         draw_button(&find_accept_button);
@@ -133,16 +163,43 @@ void _update_display_find(bool found) {
 
 void _init_display_menu() {
     to_menu_button.active = false;
-    int8_t string_buffer[30];
+    int8_t string_buffer[40];
     Graphics_setFont(&g_context, &g_sFontCm24b);
     Graphics_setForegroundColor(&g_context, GRAPHICS_COLOR_WHITE);
     Graphics_drawString(&g_context, "Menu", -1, 4, 9, false);
     Graphics_drawLineH(&g_context, 1, 320, 40);
-    sprintf((char *) &string_buffer,  "Scanning address: 0x%02x", device_address);
-    Graphics_drawStringCentered(&g_context, string_buffer, -1, 160, 90, false);
+    Graphics_setForegroundColor(&g_context, GRAPHICS_COLOR_WHITE);
+    Graphics_setFont(&g_context, &g_sFontCm20);
+
+    sprintf((char *) &string_buffer,  "Address: 0x%02x", device_address);
+    Graphics_drawString(&g_context, string_buffer, -1, 4, 50, false);
+    memset(string_buffer, 0, 40);
+
+    sprintf((char *) &string_buffer,  "SN: %d", device_id.ser_no);
+    Graphics_drawString(&g_context, string_buffer, -1, 4, 70, false);
+    memset(string_buffer, 0, 40);
+
+    sprintf((char *) string_buffer, "HW ID: ");
+    format_hw_id(string_buffer, device_id.hw_id);
+    Graphics_drawString(&g_context, string_buffer, -1, 4, 90, false);
+
+    sprintf((char *) string_buffer, "SW ID: ");
+    format_sw_id(string_buffer, device_id.sw_id);
+    Graphics_drawString(&g_context, string_buffer, -1, 4, 110, false);
+
+    sprintf((char *) &string_buffer,  "SW Ver.: %02d.%02d", (device_id.sw_ver >> 8), (char) device_id.sw_ver);
+    Graphics_drawString(&g_context, string_buffer, -1, 4, 130, false);
+    memset(string_buffer, 0, 40);
+
+    sprintf((char *) &string_buffer,  "Temp: %d°C", 23); // TODO: Replace for real value.
+    Graphics_drawString(&g_context, string_buffer, -1, 4, 150, false);
+
+    draw_button(&menu_dose_button);
+    draw_button(&menu_dose_rate_button);
+    draw_button(&menu_find_button);
 }
 
-void draw_button( button *b) {
+void draw_button(button *b) {
     b->active = true;
     Graphics_setForegroundColor(&g_context, b->button_color);
     Graphics_fillRectangle(&g_context, &(b->coords));
