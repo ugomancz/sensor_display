941bc0fb24a3d223d1eadc2887fd2db366042975
Ondřej Kostík <xkostik@fi.muni.cz>
2022-05-01 22:53:57 +0200

    Format functions format specifiers bugfix

--- mdg04.c
+++ mdg04.c
@@ -16,27 +16,27 @@ void format_hw_id(char *buffer, uint32_t id) {
     switch (type) {
     case 0x00:
         sprintf(p++, "K");
-        sprintf(p, "%d-", id / 1000);
+        sprintf(p, "%04d-", id / 1000);
         p += 5;
-        sprintf(p, "%d-", (id / 100) % 100);
+        sprintf(p, "%02d-", (id / 100) % 100);
         p += 3;
         sprintf(p++, "C");
-        sprintf(p, "%d", id % 100);
+        sprintf(p, "%02d", id % 100);
         break;
     case 0x01:
         sprintf(p, "50-A-");
         p += 5;
-        sprintf(p, "%d", id);
+        sprintf(p, "%07d", id);
         break;
     case 0x02:
         sprintf(p, "51-A-");
         p += 5;
-        sprintf(p, "%d", id);
+        sprintf(p, "%07d", id);
         break;
     case 0x03:
         sprintf(p, "52-A-");
         p += 5;
-        sprintf(p, "%d", id);
+        sprintf(p, "%07d", id);
         break;
     }
 }
@@ -49,31 +49,31 @@ void format_sw_id(char *buffer, uint32_t id) {
     switch (type) {
     case 0x00:
         sprintf(p++, "K");
-        sprintf(p, "%d-", id / 1000);
+        sprintf(p, "%04d-", id / 1000);
         p += 5;
-        sprintf(p, "%d-", (id / 100) % 100);
+        sprintf(p, "%02d-", (id / 100) % 100);
         p += 3;
         sprintf(p++, "F");
-        sprintf(p, "%d", id % 100);
+        sprintf(p, "%02d", id % 100);
         break;
     case 0x04:
         sprintf(p, "53-A-");
         p += 5;
-        sprintf(p, "%d", id);
+        sprintf(p, "%07d", id);
         break;
     case 0x05:
         sprintf(p, "54-A-");
         p += 5;
-        sprintf(p, "%d", id);
+        sprintf(p, "%07d", id);
         break;
     case 0x08:
         sprintf(p++, "S");
-        sprintf(p, "%d-", id / 10000);
+        sprintf(p, "%04d-", id / 10000);
         p += 4;
-        sprintf(p, "%d-", (id / 100) % 100);
+        sprintf(p, "%02d-", (id / 100) % 100);
         p += 3;
         sprintf(p++, "F");
-        sprintf(p, "%d", id % 100);
+        sprintf(p, "%02d", id % 100);
         break;
     }
 }
